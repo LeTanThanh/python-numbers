b4633b404225c6f1d92aee05ee04b7c579bd0f40
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-22 17:20:10 +0700

    Add main.py

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Python Numbers
 
 - Reference: https://www.pythontutorial.net/python-basics/python-numbers/
+
+- Run scripts
+```python
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+  # Integers
+  print(20 + 10)        # 30
+  print(20 - 10)        # 10
+  print(20 * 10)        # 200
+  print(20 / 10)        # 2.0
+  print(3 ** 3)         # 27
+  print(20 / (10 + 10)) # 1.0
+
+  # Floats
+  print(0.5 + 0.5)  # 1.0
+  print(0.5 - 0.5)  # 0.0
+  print(0.5 / 0.5)  # 1.0
+  print(0.5 * 0.5)  # 0.25
+  print(20 / 10)    # 2.0
+  print(1 + 2.0)    # 3.0
+  print(0.1 + 0.2)  # 0.30000000000000004
+
+  # Underscores in numbers
+  count = 10_000_000_000
+  print(count)
